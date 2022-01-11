ee1610c110987f923642467ff6476bb261f482a8
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-11 18:51:03 -0500

    get endpoints for post comments complete and tested

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -31,22 +31,41 @@ router.get("/api/posts/:id", async (req, res) => {
   }
 });
 
-router.post("/api/posts", (req, res) => {});
+// router.post("/api/posts", (req, res) => {});
 
-router.put("/api/posts/:id", (req, res) => {});
+// router.put("/api/posts/:id", (req, res) => {});
 
 router.delete("/api/posts/:id", async (req, res) => {
   try {
-    const post = await Posts.remove(req.params.id);
-    if (!post) {
+    const find = await Posts.findById(req.params.id);
+    if (!find) {
       res
         .status(404)
         .json({ message: "The post with the specified ID does not exist" });
     } else {
-      res.json(post);
+      await Posts.remove(req.params.id);
+      res.json(find);
     }
   } catch (err) {
     res.status(500).json({ message: "The post could not be removed" });
+  }
+});
+
+router.get("/api/posts/:id/comments", async (req, res) => {
+  try {
+    const comments = await Posts.findPostComments(req.params.id);
+    const commentById = await Posts.findCommentById(req.params.id);
+    if (!commentById) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else {
+      res.json(comments);
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The comments information could not be retrieved" });
   }
 });
 
